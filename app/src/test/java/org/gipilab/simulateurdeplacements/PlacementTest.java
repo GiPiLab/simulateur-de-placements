fa71ea6bf975a17e8d77ebbcecc9826b0cd374db
Thibault <thibault@gipilab.org>
2016-02-23 11:30:58 +0100

    Add tests for mensualitesToAnnualites

--- app/src/test/java/org/gipilab/simulateurdeplacements/PlacementTest.java
+++ app/src/test/java/org/gipilab/simulateurdeplacements/PlacementTest.java
@@ -14,6 +14,47 @@ public class PlacementTest {
 
     @Test
     public void testMensualitesToAnnualites() throws Exception {
+        Placement p = new Placement();
+        p.setCapitalInitial(BigDecimal.valueOf(10000));
+        p.setTauxAnnuel(new BigDecimal("0.12"));
+        p.setFrequenceVariation(enumFrequenceVariation.MENSUELLE);
+        p.setVariation(BigDecimal.valueOf(100));
+        p.setDuree(25);
+
+        ArrayList<Mensualite> tableau = p.tableauPlacement();
+        ArrayList<Annualite> annualites = Placement.mensualitesToAnnualites(tableau);
+        Assert.assertEquals(3, annualites.size());
+        Annualite a = annualites.get(0);
+        Assert.assertEquals(1, a.getIeme());
+        Assert.assertEquals(new BigDecimal("10000"), a.getCapitalPlaceDebutAnnee());
+        Assert.assertEquals(new BigDecimal("11100"), a.getCapitalPlaceFinAnnee());
+        Assert.assertEquals(new BigDecimal("12366.00"), a.getValeurAcquiseFinAnnee());
+        Assert.assertEquals(new BigDecimal("1266.00"), a.getInteretsFinAnnee());
+        Assert.assertEquals(new BigDecimal("1266.00"), a.getInteretsTotaux());
+        Assert.assertEquals(12, a.getMensualites().size());
+
+        a = annualites.get(1);
+        Assert.assertEquals(2, a.getIeme());
+        Assert.assertEquals(new BigDecimal("12366.00"), a.getCapitalPlaceDebutAnnee());
+        Assert.assertEquals(new BigDecimal("13566.00"), a.getCapitalPlaceFinAnnee());
+        Assert.assertEquals(new BigDecimal("15127.9200"), a.getValeurAcquiseFinAnnee());
+        Assert.assertEquals(new BigDecimal("1561.9200"), a.getInteretsFinAnnee());
+        Assert.assertEquals(new BigDecimal("2827.9200"), a.getInteretsTotaux());
+        Assert.assertEquals(12, a.getMensualites().size());
+
+        a = annualites.get(2);
+        Assert.assertEquals(3, a.getIeme());
+        Assert.assertEquals(new BigDecimal("15127.9200"), a.getCapitalPlaceDebutAnnee());
+        Assert.assertEquals(new BigDecimal("15227.9200"), a.getCapitalPlaceFinAnnee());
+        Assert.assertEquals(new BigDecimal("15380.199200"), a.getValeurAcquiseFinAnnee());
+        Assert.assertEquals(new BigDecimal("152.279200"), a.getInteretsFinAnnee());
+        Assert.assertEquals(new BigDecimal("2980.199200"), a.getInteretsTotaux());
+        Assert.assertEquals(1, a.getMensualites().size());
+
+
+
+
+
 
     }
 
@@ -62,6 +103,7 @@ public class PlacementTest {
         Assert.assertEquals(new BigDecimal("152.279200"), mens.getInteretsObtenus());
         Assert.assertEquals(new BigDecimal("2980.199200"), mens.getInteretsTotaux());
 
+        //Test avec variation trimestrielle
         p.setFrequenceVariation(enumFrequenceVariation.TRIMESTRIELLE);
         tableau = p.tableauPlacement();
 
@@ -98,11 +140,5 @@ public class PlacementTest {
         Assert.assertEquals(BigDecimal.valueOf(100), mens.getVariation());
         Assert.assertEquals(new BigDecimal("134.301600"), mens.getInteretsObtenus());
         Assert.assertEquals(new BigDecimal("2764.461600"), mens.getInteretsTotaux());
-
-
-
-
-
-
     }
 }
